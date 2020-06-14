819fdd098ee7c5ecb3077c49d985254524e8c36d
Luís Rodrigues <luis.cnm@gmail.com>
2020-06-14 23:54:42 +0100

    Finished async challenge + added es6 content

--- JS/asyncJS/main.js
+++ JS/asyncJS/main.js
@@ -1,3 +1,4 @@
+/*
 var xhr = new XMLHttpRequest();
 
 // part 1 - get github api info
@@ -31,11 +32,11 @@ var myPromise = function () {
     });
 }
 
-/*
+
     if: 
         console.log(result); it returns "Pending"
         because it is executed first 
-*/
+
 
 myPromise()
     .then(function(response) {
@@ -54,5 +55,72 @@ axios.get('https://api.github.com/users/luisrodrigues')
     .catch(function(error) {
         console.warn(error);
     });
+*/
 
+// challenge 1
 
+var checkAge = function(age) {
+    return new Promise(function(resolve, reject){
+        setInterval(function(){
+            if (age > 18) {
+                resolve();
+            } else {
+                reject();
+            }
+        }, 2000);
+    });
+}
+
+checkAge(20)
+    .then(function(){
+        console.log('Over 18 yo');
+    })
+    .catch(function(){
+        console.warn('Younger than 18');
+    });
+
+// challenge 2
+
+var inputElement = document.querySelector('#app input');
+var btnElement = document.querySelector('#app button');
+var listElement = document.querySelector('#app ul');
+
+btnElement.onclick = function() {
+    var username = inputElement.value;
+    listElement.innerHTML = '';
+
+    var itemElement = document.createElement('li');
+    var itemText = document.createTextNode('Loading...');
+            
+    itemElement.appendChild(itemText);
+    listElement.appendChild(itemElement);
+
+    axios.get('https://api.github.com/users/' + username + '/repos')
+        .then(function(response) {
+            listElement.innerHTML = '';
+            var repos = response.data || [];
+            if (repos.length > 0) {
+                for (repo of repos) {
+                    var repoElement = document.createElement('li');
+                    var repoNameText = document.createTextNode(repo.name);
+                
+                    repoElement.appendChild(repoNameText);
+                    listElement.appendChild(repoElement); 
+                }
+            } else {
+                var itemElement = document.createElement('li');
+                var itemText = document.createTextNode('No repositories have been created');
+                        
+                itemElement.appendChild(itemText);
+                listElement.appendChild(itemElement); 
+            }       
+        })
+        .catch(function(error) {
+            listElement.innerHTML = '';
+            var itemElement = document.createElement('li');
+            var itemText = document.createTextNode('User does not exist');
+            
+            itemElement.appendChild(itemText);
+            listElement.appendChild(itemElement);
+        });
+}
