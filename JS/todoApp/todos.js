9ad6093ccbca25345d56c8fd2b701be2930a3d56
Luís Rodrigues <luis.cnm@gmail.com>
2020-06-14 12:18:44 +0100

    Finished TodoApp + AsyncJS

---
+++ JS/todoApp/todos.js
@@ -0,0 +1,53 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+// !!!
+var todos = JSON.parse(localStorage.getItem('todoList')) || [];
+
+function renderTodos() {
+    listElement.innerHTML = '';
+
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo + " ");
+
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+        
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+        var linkText = document.createTextNode('[Delete]');
+
+        linkElement.appendChild(linkText);        
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
+
+function addTodo() {
+    var todoText = inputElement.value;
+    
+    if(todoText !== '') {
+        todos.push(todoText);
+        inputElement.value = '';
+        renderTodos();
+        saveTodos();
+    }
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
+    renderTodos();
+    saveTodos();
+}
+
+// !!!
+function saveTodos() {
+    localStorage.setItem('todoList', JSON.stringify(todos));
+}
