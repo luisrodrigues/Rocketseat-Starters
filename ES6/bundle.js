b9b3d8f6ae1492061291b78d3965a5a178d3ed28
Luís Rodrigues <luis.cnm@gmail.com>
2020-06-15 22:29:19 +0100

    es6 content: classes

--- ES6/bundle.js
+++ ES6/bundle.js
@@ -6,20 +6,56 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-alert('Test');
+// es6 - classes
 
-var TestClass = /*#__PURE__*/function () {
-  function TestClass() {
-    _classCallCheck(this, TestClass);
+/*
+class List {
+    constructor() {
+        this.data = [];
+    }
+
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+
+//inheritance
+class TodoList extends List {
+    constructor() {
+        super();
+
+        this.user = 'Luis';
+    }
+
+    showUser() {
+        console.log(this.user);
+    }
+}
+
+var myTodoList = new TodoList();
+
+document.getElementById('newTodo').onclick = function() {
+    myTodoList.add('new todo');
+}
+
+myTodoList.showUser();
+*/
+var Maths = /*#__PURE__*/function () {
+  function Maths() {
+    _classCallCheck(this, Maths);
   }
 
-  _createClass(TestClass, [{
-    key: "method",
-    value: function method() {}
-  }, {
-    key: "otherMethod",
-    value: function otherMethod() {}
+  _createClass(Maths, null, [{
+    key: "sum",
+    // static mehods: can't be called in class intances
+    // mostly used for util methods
+    value: function sum(a, b) {
+      return a + b;
+    }
   }]);
 
-  return TestClass;
+  return Maths;
 }();
+
+console.log(Maths.sum(1, 2));
--- ES6/main.js
+++ ES6/main.js
@@ -1,11 +1,49 @@
-alert('Test');
+// es6 - classes
 
-class TestClass {
-    method() {
-
+/*
+class List {
+    constructor() {
+        this.data = [];
     }
 
-    otherMethod() {
-        
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+//inheritance
+class TodoList extends List {
+    constructor() {
+        super();
+
+        this.user = 'Luis';
+    }
+
+    showUser() {
+        console.log(this.user);
+    }
+}
+
+var myTodoList = new TodoList();
+
+document.getElementById('newTodo').onclick = function() {
+    myTodoList.add('new todo');
+}
+
+myTodoList.showUser();
+*/
+
+class Maths {
+    // static mehods: can't be called in class intances
+    // mostly used for util methods
+    static sum(a, b) {
+        return a + b;
+    }
+}
+
+console.log(Maths.sum(1, 2));
+
+
+
+
