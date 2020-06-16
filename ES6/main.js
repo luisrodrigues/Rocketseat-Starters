13c8cdc4aa173c858678306924a6434bc5cd2bdf
Luís Rodrigues <luis.cnm@gmail.com>
2020-06-16 18:27:20 +0100

    Array Operations

--- ES6/main.js
+++ ES6/main.js
@@ -60,6 +60,33 @@ function test(x) {
 }
 // console.log(y); would not work here
 test(10);
-*/
 
 // Array Operations
+const arr = [1, 3, 4, 5, 9, 2];
+
+// map
+const newArr = arr.map(function name(item) {
+  return item * 2;
+});
+console.log(newArr);
+
+// reduce
+const sum = arr.reduce(function name(total, next) {
+  return total + next;
+});
+console.log(sum);
+
+// filter
+const filterArr = arr.filter(function name(item) {
+  return item % 2 === 0;
+});
+console.log(filterArr);
+
+// find
+const findArr = arr.find(function name(item) {
+  return item === 4; // returns undefined if not found
+});
+console.log(findArr);
+*/
+
+// Arrow Functions
