799b67382b3111d5f6fd00004b12980868cf32b9
Luís Rodrigues <luis.cnm@gmail.com>
2020-06-17 14:13:52 +0100

    Async/await
    Executed:
    yarn add @babel/plugin-transform-async-to-generator -D
    yarn add @babel/polyfill -D

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -4,9 +4,29 @@
 console.log(functions.add(1, 7));
 console.log(functions.sub(4, 2));
 console.log(functions.mult(4, 2));
-*/
+
 // challenge
 import User, { age as userAge } from "./functions";
 
 User.info();
 console.log(userAge);
+*/
+// Async/await
+const myPromise = () =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      resolve("OK");
+    }, 2000)
+  );
+
+// myPromise()
+//  .then((response) => console.log(response))
+//  .catch((err) => console.warn(err));
+const executePromise = async () => {
+  console.log(await myPromise());
+  console.log(await myPromise());
+  console.log(await myPromise());
+};
+// easier than cascade
+executePromise();
+// above is also a promise
