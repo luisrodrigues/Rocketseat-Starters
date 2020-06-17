a6ae2711c2947bad94b49200387befab488e889e
Luís Rodrigues <luis.cnm@gmail.com>
2020-06-17 11:41:25 +0100

    Import/Export statements

---
+++ ES6/add.js
@@ -0,0 +1,8 @@
+// Import / Export Statements - pt2
+// 'add' is exported by default and can be
+// directly named differently at the destination
+
+// there cannot be more than 1 export default
+export default function add(a, b) {
+  return a + b;
+}
--- ES6/functions.js
+++ ES6/functions.js
@@ -1,3 +1,12 @@
+// Import / Export Statements - pt1
 export function add(a, b) {
   return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+export function mult(a, b) {
+  return a * b;
 }
