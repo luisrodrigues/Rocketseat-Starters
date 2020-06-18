758f1a029e62016a05d35d7ab0c9bb7afcacb995
Luís Rodrigues <luis.cnm@gmail.com>
2020-06-18 10:48:24 +0100

    ES6 App - add repositories

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -1,1 +1,24 @@
+class App {
+  constructor() {
+    this.repositories = [];
+    this.formEl = document.getElementById("repo-form");
+    this.registerHandlers();
+  }
+  // registers events
+  registerHandlers() {
+    this.formEl.onsubmit = (event) => this.addRepository(event);
+  }
+  addRepository(event) {
+    // stops page from reloading after submission
+    event.preventDefault();
+    this.repositories.push({
+      name: "rocketseat.com.br",
+      description: "description text",
+      avatar_url: "https://avatars0.githubusercontent.com/u/28929274?v=4",
+      html_url: "https://github.com/rocketseat/rocketseat.com.br",
+    });
+    console.log(this.repositories);
+  }
+}
 
+new App();
