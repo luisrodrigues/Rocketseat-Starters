393ee86d7bba1624eba8ed1b778381b524292672
Luís Rodrigues <luis.cnm@gmail.com>
2020-06-19 12:22:55 +0100

    Api: Adding a base route

--- node-api/server.js
+++ node-api/server.js
@@ -1,6 +1,10 @@
 // api
 const express = require("express");
-
+// initializing express
 const app = express();
-
+// base route
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+// start server on port
 app.listen(3001);
