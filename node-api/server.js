9a46a54a407b5519f6e4c5a86f2658735b74b857
Luís Rodrigues <luis.cnm@gmail.com>
2020-06-20 12:15:32 +0100

    API: added mongoDB connection
    Executed:
    npm install mongoose
    npm install require-dir

--- node-api/server.js
+++ node-api/server.js
@@ -1,10 +1,32 @@
 // api
 const express = require("express");
+// non-relational ORM
+const mongoose = require("mongoose");
+// package to ease requiring
+const requireDir = require("require-dir");
+
 // initializing express
 const app = express();
+// DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, // added due to warning
+});
+requireDir("./src/models");
+
+//test
+const Product = mongoose.model("Product");
+
 // base route
 app.get("/", (req, res) => {
-  res.send("hello bro");
+  // test
+  Product.create({
+    title: "React",
+    description: "frontend framework",
+    url: "https://reactjs.org/",
+  });
+
+  return res.send("hello bro");
 });
 // start server on port
 app.listen(3001);
