a1196d65c7cb8b593ca496928b6e0bfa6f219f67
Luís Rodrigues <luis.cnm@gmail.com>
2020-06-22 00:12:46 +0100

    ReactJS: listed products (from api)

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import api from "../../services/api";
 
+// statefull component
 export default class Main extends Component {
+  // react variable
+  state = {
+    products: [],
+  };
+
   componentDidMount() {
     this.loadProducts();
   }
   // user functions need to be arrowed
   loadProducts = async () => {
     const response = await api.get("/products");
-    console.log(response.data.docs);
+    // react method
+    this.setState({ products: response.data.docs });
   };
 
   render() {
-    return <h1>Hello Api</h1>;
+    return (
+      <div className="product-list">
+        {this.state.products.map((product) => (
+          // when using map: key is required
+          <h2 key={product._id}>{product.title}</h2>
+        ))}
+      </div>
+    );
   }
 }
