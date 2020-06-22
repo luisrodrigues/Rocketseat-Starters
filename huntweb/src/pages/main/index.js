0b1f2ce8b2f42cfc2c7a60d61c1074165e55033e
Luís Rodrigues <luis.cnm@gmail.com>
2020-06-22 15:45:47 +0100

    ReactJS: added prev/next buttons

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -7,20 +7,41 @@ export default class Main extends Component {
   // react variable
   state = {
     products: [],
+    productInfo: {},
+    page: 1,
   };
 
   componentDidMount() {
     this.loadProducts();
   }
   // user functions need to be arrowed
-  loadProducts = async () => {
-    const response = await api.get("/products");
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
+
+    const { docs, ...productInfo } = response.data;
+
     // react method
-    this.setState({ products: response.data.docs });
+    this.setState({ products: docs, productInfo, page });
+  };
+
+  prevPage = () => {
+    const { page } = this.state;
+    if (page === 1) return;
+
+    const pageNumber = page - 1;
+    this.loadProducts(pageNumber);
+  };
+
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+    if (page === productInfo.pages) return;
+
+    const pageNumber = page + 1;
+    this.loadProducts(pageNumber);
   };
 
   render() {
-    const { products } = this.state;
+    const { products, productInfo, page } = this.state;
 
     return (
       <div className="product-list">
@@ -29,9 +50,17 @@ export default class Main extends Component {
           <article key={product._id}>
             <strong>{product.title}</strong>
             <p>{product.description}</p>
-            <a href="">Website</a>
+            <a href="">About</a>
           </article>
         ))}
+        <div className="actions">
+          <button disabled={page === 1} onClick={this.prevPage}>
+            Previous
+          </button>
+          <button disabled={page === productInfo.pages} onClick={this.nextPage}>
+            Next
+          </button>
+        </div>
       </div>
     );
   }
